f300eefa85884774ce97d6f39b5ce34a11be61b3
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-23 11:40:55 +0530

    mergin throuv while loop and measurement space and time complexity

--- Array/#5-6-Search-&-merge.js
+++ Array/#5-6-Search-&-merge.js
@@ -1,4 +1,4 @@
-//!  ========= #4 Searching Element ===========
+//!  ========= #5 Searching Element ===========
 /*
   // Manual
     const arr = [23,69, 44, 56, 76, 43, 69, 34, 35];
@@ -16,20 +16,54 @@
 
   console.log(arr.indexOf(searchValue))
 
-*/
-//!  ========= #5 Merge Two Array ===========
 
-  // Manual
-      const arr1 = [24, 54, 64, 23, 43];
-      const arr2 = [23, 24, 32, 43, 54];
-      const mergeArr = [];
+//!  ========= #6 Merge Two Array (For Loop)(Not Sorted) ===========
+
+// Manual
+      const arr1 = [24, 54, 64, 77, 95, 97, 99];
+      const arr2 = [7, 45, 78, 87, 89];
+      const mergeArr2 = [];
 
       for (let i = 0; i < arr1.length; i++) {
-        mergeArr[i] = arr1[i];
+        mergeArr2[i] = arr1[i];
       }
 
       for (let i = 0; i < arr2.length; i++) {
-        mergeArr[arr1.length + i] = arr2[i];
+        mergeArr2[arr1.length + i] = arr2[i];
       }
 
-      console.log(mergeArr);
+      console.log(mergeArr2);
+*/
+//!  ========= #7 Merge Two Array (While Loop)(Sorted) ===========
+
+const data1 = [24, 54, 64, 77, 95, 97, 99];
+const data2 = [7, 45, 78, 87, 89];
+let mergeArr3 = [];
+
+let i = 0;
+let j = 0;
+let k = 0;
+
+while (i < data1.length && j < data2.length) {
+  if (data1[i] < data2[j]) {
+    mergeArr3[k] = data1[i];
+    i++;
+  } else {
+    mergeArr3[k] = data2[j];
+    j++;
+  }
+  k++;
+}
+
+while (i < data1.length) {
+  mergeArr3[k] = data1[i];
+  i++;
+  k++;
+}
+while (j < data2.length) {
+  mergeArr3[k] = data2[j];
+  j++;
+  k++;
+}
+
+console.log(mergeArr3);
---
+++ Array/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+ < data1.length) {
+//   mergeArr3[k] = data1[i + 1];
+//   i++;
+//   k++;
+// }
+
+// consol
