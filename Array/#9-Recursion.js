942d78626823e84a892b095c76350b8d702517f2
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-26 09:45:20 +0530

    basic recursion

---
+++ Array/#9-Recursion.js
@@ -0,0 +1,21 @@
+//! Recursion
+
+//* Normal Recursion
+let value = 0;
+
+const recursion = (x) => {
+  console.log(x);
+  if (x < 10) {
+    recursion(x + 1);
+  }
+};
+
+// recursion(value); // --> 1 - 10 tak print
+
+//* Factorial Recursion => 5! -> 5*4*3*2*1 = 120
+const factorial = (n) => {
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
+};
+
+console.log(factorial(5));
