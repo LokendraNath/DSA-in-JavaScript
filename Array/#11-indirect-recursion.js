5f5115a56414347c24c7f1ee16a9fbd65c3943d9
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-27 10:09:59 +0530

    learn about indirect recursion

---
+++ Array/#11-indirect-recursion.js
@@ -0,0 +1,18 @@
+let money = 1000;
+let totalBanana = 0;
+
+function buyBanana(x) {
+  if (x > 0) {
+    console.log(`I have ${x}Rs. And ${totalBanana} Banana`);
+    buyMore(x);
+  } else {
+    console.log("I don't have Money", totalBanana);
+  }
+}
+
+function buyMore(x) {
+  totalBanana++;
+  buyBanana(x - 100);
+}
+
+buyBanana(money);
